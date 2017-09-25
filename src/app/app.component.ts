545336be815221626558aeeaaf1cd69dd857deee
Marcos R Oliveira <mroger.oliveira@gmail.com>
2017-09-24 21:09:51 -0300

    Using inline template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+            <app-header></app-header>
+            <router-outlet></router-outlet>
+            `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
 
-  constructor() {}  
+  constructor() {}
 
 }
